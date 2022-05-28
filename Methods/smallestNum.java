2d56e0a254b6856093b254a5172688f3feba0fa0
anigonzales <anisgonzales@gmail.com>
2022-05-28 13:16:13 -0500

    smallestnum from w3

--- Methods/smallestNum.java
+++ Methods/smallestNum.java
@@ -1,22 +1,27 @@
+
 // Write a Java method to find the smallest number among three numbers.
 
 import java.util.Scanner;
+
 public class smallestNum {
 
- public static void main(String[] args)
-    {
+ public static void main(String[] args){
+
         Scanner in = new Scanner(System.in);
         System.out.print("Input the first number: ");
+
         double x = in.nextDouble();
         System.out.print("Input the Second number: ");
+
         double y = in.nextDouble();
         System.out.print("Input the third number: ");
+
         double z = in.nextDouble();
         System.out.print("The smallest value is " + smallest(x, y, z)+"\n" );
     }
 
-   public static double smallest(double x, double y, double z)
-    {
+   public static double smallest(double x, double y, double z){
         return Math.min(Math.min(x, y), z);
     }
+
 }
